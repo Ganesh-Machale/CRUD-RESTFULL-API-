27a4a499a85075be1911a698576d10fd2c7d7d17
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-03-23 09:10:39 +0530

    created the route to delete a specific post its id

--- index.js
+++ index.js
@@ -78,7 +78,14 @@ app.use(methodOverride('_method'));
           let {id} = req.params;
            let post  = posts.find((p)=> id === p.id);
           res.render("edit.ejs",{post});
-        })
+        });
+
+        // creating this route for deleting for a specific post
+         app.delete("/posts/:id",(req,res)=>{
+          let {id} = req.params;
+           posts  = posts.filter((p)=> id !== p.id);
+          res.redirect("/posts");
+         })
       
  
    app.listen(port,(req,res)=>{
