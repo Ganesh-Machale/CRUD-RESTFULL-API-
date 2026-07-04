b11e2a570a1c8f027bbb458075a6f4ac0ad2f4d0
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-07-04 18:59:44 +0530

    get request to render the data

---
+++ README.md
@@ -0,0 +1,2 @@
+# craeting RESTFULL APIS :
+  1) rendering data with the help of GET request 
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+  const path = require("path");
+
+  app.use(express.urlencoded({extended:true}));
+   app.use(express.json());
+
+  app.set("view engine","ejs");
+  app.set("views",path.join(__dirname,"views"));
+
+    app.use(express.static(path.join(__dirname,"public")));
+
+    let posts =[
+    {
+      username:"Ganesh",
+      content:"i love coding" 
+    },
+    {
+       username:"Nanduraj",
+       content:"i like to play BGMI"
+    },
+    {
+      username:"Atharv",
+      content:"i love to view Reels on instagram"
+        },
+  ];
+
+     app.get("/",(req,res)=>{
+        res.send("Server Working Well");
+     });
+    
+     app.get("/posts",(req,res)=>{
+      res.render("index.ejs",{posts});
+     })
+
+   app.listen(port,(req,res)=>{
+    console.log("Port is Listening to ",port);
+   })
